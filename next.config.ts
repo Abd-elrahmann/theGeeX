dfd67fb29da51b0662716ddbd81134f8c31f66f9
Abd-elrahmann <cds.abdelrahmanmohamed74057@alexu.edu.eg>
2026-07-29 16:34:00 +0300

    fix: update allowedDevOrigins and add iOS Safari detection in navbar state

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["127.0.0.1", "localhost", "192.168.1.66"],
+  allowedDevOrigins: ["127.0.0.1","192.168.1.47"," http://192.168.1.47:3000"],
   async headers() {
     return [
       {
