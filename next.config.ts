a2b424529fd7a86948c56e47146d30fe7780a86b
Abd-elrahmann <cds.abdelrahmanmohamed74057@alexu.edu.eg>
2026-07-29 17:19:35 +0300

    refactor: remove unused headers configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,28 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1","192.168.1.47"," http://192.168.1.47:3000"],
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; base-uri 'self'; frame-ancestors 'none'; object-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; font-src 'self' data: https:; connect-src 'self' https:; media-src 'self' https:; form-action 'self'; upgrade-insecure-requests",
-          },
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
